022afa8407387df3f4705387a376b608dc09af35
fianter marpaung <fiantermr>
2022-11-04 23:42:16 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing</title>
+    <link rel="stylesheet" href="style.css">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
+</head>
+
+<body>
+    <form class="">
+        <div class="title">
+            <h2>Personal Details</h2>
+        </div>
+        <div class="half">
+            <div class="item">
+                <label>Wanted Job Title<i class="fa fa-question-circle"></i></label>
+                <input type="text" required value="">
+            </div>
+            <div class="item">
+                <div class="avatar"></div>
+            </div>
+        </div>
+        <div class="half">
+            <div class="item">
+                <label>First Name</label>
+                <input type="text" required value="">
+            </div>
+            <div class="item">
+                <label>Last Name</label>
+                <input type="text" required value="">
+            </div>
+        </div>
+        <div class="half">
+            <div class="item">
+                <label>Email</label>
+                <input type="text" required value="">
+            </div>
+            <div class="item">
+                <label>Email</label>
+                <select class="country">
+                    <option value="country">Select Country</option>
+                    <option value="indonesia">Indonesia</option>
+                    <option value="australia">Australia</option>
+                    <option value="amerika">Amerika Serikat</option>
+                    <option value="germany">Germany</option>
+                </select>
+            </div>
+
+        </div>
+        <div class="half">
+            <div class="item">
+                <label>Email</label>
+                <input type="text" required value="">
+            </div>
+            <div class="item">
+                <label>Password</label>
+                <input type="password" required value="">
+                <datalist id="cars">
+                    <option>Volvo</option>
+                    <option>Saab</option>
+                    <option>Mercedes</option>
+                    <option>Audi</option>
+                </datalist>
+            </div>
+        </div>
+        <div class="full">
+            <div class="item">
+                <label>Phone</label>
+                <input type="text" required value="">
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
